287516c326c1c2cdd1f9453094c4a85398715571
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-04-01 18:37:28 +0000

    refactor : abstracting logic to separate modules and files

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,64 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import { Result } from "@/types/result";
+import { generateResult } from "@/lib/result";
+import { encodeData, decodeData } from "@/lib/encode";
+
+import FriendCard from "@/components/FriendCard";
+import InputForm from "@/components/InputForm";
+import ResultCard from "@/components/ResultCard";
+
+type View = "friend" | "input" | "result";
+
+export default function Page() {
+  const [view, setView] = useState<View>("input");
+  const [friendData, setFriendData] = useState<Result | null>(null);
+  const [result, setResult] = useState<Result | null>(null);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const data = params.get("data");
+
+    if (data) {
+      const decoded = decodeData(data);
+      if (decoded) {
+        setFriendData(decoded);
+        setView("friend");
+      }
+    }
+  }, []);
+
+  const handleSubmit = (name: string) => {
+    const res = generateResult(name);
+    setResult(res);
+    setView("result");
+  };
+
+  const handleShare = () => {
+    if (!result) return;
+
+    const encoded = encodeData(result);
+    const link = `${window.location.origin}?data=${encoded}`;
+
+    navigator.clipboard.writeText(link);
+    alert("Copied 🔥");
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="flex items-center justify-center h-screen">
+      {view === "friend" && friendData && (
+        <FriendCard
+          data={friendData}
+          onContinue={() => setView("input")}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      )}
+
+      {view === "input" && <InputForm onSubmit={handleSubmit} />}
+
+      {view === "result" && result && (
+        <ResultCard data={result} onShare={handleShare} />
+      )}
     </div>
   );
 }
